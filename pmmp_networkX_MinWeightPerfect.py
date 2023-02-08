b149ea301d5ac329b592580a3a90a3eb1fda9843
Daphne-Skiado <daphne.skiado@gmail.com>
2023-02-08 11:53:13 +0100

    Fixed python script

--- pmmp_networkX_MinWeightPerfect.py
+++ pmmp_networkX_MinWeightPerfect.py
@@ -44,11 +44,6 @@ for index, sample in samples.iterrows():
     V = A + D
     keys = [key for key in modifications_num_dict]
     V_names = ['site '+str(i) for i in A] + ['mod '+str(i)+' occ '+str(j+1) for i in range(len(keys)) for j in     range(modifications_num_dict[keys[i]])]
-    
-    #Add dummy vertices to make a perfect matching possible
-    if len(D) < len(A):
-    	dummy_vertices = ['dummy_mod_' + str(i) for i in range(len(A)-len(D))]
-    	V_names = V_names + dummy_vertices
     	
     #print('Vertices: ', len(V_names))
     #for v in V_names:
@@ -69,29 +64,11 @@ for index, sample in samples.iterrows():
     weights[weights == 0.0] = 0.01
     normalized_weights = weights / np.linalg.norm(weights)
     max_weight = max(normalized_weights) + 0.5
-    reverted_weights = np.array([max(normalized_weights)-w for w in normalized_weights])
-    reverted_weights[reverted_weights == 0.0] = 0.01
+    reverted_weights = np.array([max_weight-w for w in normalized_weights])
 
     for i in range(len(E)):
     	edge = E[i]
     	edge[2] = reverted_weights[i]
- 
-    #Make graph complete
-    # add edges from/to dummy vertices
-    if len(D) < len(A):
-    	for a_vertex_index in range(len(A)):
-    		for dummy_vertex_index in range(len(A) + len(D), len(V_names)):
-    			dummy_edge = [V_names[a_vertex_index], V_names[dummy_vertex_index], max_weight]
-    			E.append(dummy_edge)
-    
-    #Add missing edges from other vertices
-    for v1_index in range(len(A)):
-    	vertices_connected_to_v1 = [e[1] for e in E if (e[0] == V_names[v1_index])]
-    	for v2_index in range(len(A)+1, len(A) + len(D)):
-    		if V_names[v2_index] in vertices_connected_to_v1:
-    			continue
-    		else:
-    			edge = [V_names[v1_index], V_names[v2_index], max_weight]
 
     #check if the graph does not satisfy requirements
     try:
@@ -138,12 +115,8 @@ for index, sample in samples.iterrows():
         if vertex1 in matched or vertex2 in matched:
             continue
         match_info = [sample['spectrum_key'].iloc[0], sample['peptide_key'].iloc[0]]
-        flag_dummy_vertex = False
         for e in E:
             if vertex1 in e and vertex2 in e:
-                if 'dummy' in vertex1 or 'dummy' in vertex2:
-                    flag_dummy_vertex = True
-                    break
                 matching_weights_sum += e[2]
                 if 'site' in vertex1:
                     matched.append(vertex1)
@@ -153,9 +126,6 @@ for index, sample in samples.iterrows():
                 else:
                     match_info.append(int(vertex2.split(' ')[1]))
                     match_info.append(D[V_names.index(vertex1)-len(A)])
-        if flag_dummy_vertex == True:
-        	continue
-        print(match_info)
         matchings_info.append(match_info)
         
     #Draw the grpah
@@ -172,7 +142,7 @@ for index, sample in samples.iterrows():
 results_df = pd.DataFrame(matchings_info, columns = ['spectrum_key', 'peptide_key', 'modification_site', 'modification_mass'])
 print(results_df)
 print('\n')
-results_df.to_csv('./matchings_phospho.csv', index=False, sep ='\t')
+results_df.to_csv('./matchings_phospho_mwp_02.csv', index=False, sep ='\t')
 end = time.time()
 print('Total running time : ', end-start)
 
